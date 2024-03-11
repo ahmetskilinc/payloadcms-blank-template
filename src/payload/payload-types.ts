ed9e7f2fb35a0d458995748bce7f67e65aeb0536
Ahmet Kilinc <akx9@icloud.com>
2024-03-11 23:40:04 +0000

    update small parts

--- src/payload/payload-types.ts
+++ src/payload/payload-types.ts
@@ -10,41 +10,22 @@ export interface Config {
   collections: {
     users: User;
     pages: Page;
-    'blog-posts': BlogPost;
-    'blog-categories': BlogCategory;
-    partners: Partner;
     media: Media;
-    files: File;
-    'newsletter-contacts': NewsletterContact;
-    messages: Message;
     'payload-preferences': PayloadPreference;
     'payload-migrations': PayloadMigration;
   };
-  globals: {
-    meta: Meta;
-    megamenu: Megamenu;
-    footer: Footer;
-    'social-media': SocialMedia;
-  };
+  globals: {};
 }
+/**
+ * This interface was referenced by `Config`'s JSON-Schema
+ * via the `definition` "users".
+ */
 export interface User {
   id: string;
   firstname: string;
   lastname: string;
-  profileimage?: string | Media | null;
-  companyrole: 'cto-director' | 'ceo-director' | 'fe-developer' | 'be-developer';
-  socialmedia?:
-    | {
-        icon?: ('instagram' | 'facebook' | 'youtube' | 'linkedin' | 'twitter' | 'github') | null;
-        href?: string | null;
-        id?: string | null;
-      }[]
-    | null;
   updatedAt: string;
   createdAt: string;
-  enableAPIKey?: boolean | null;
-  apiKey?: string | null;
-  apiKeyIndex?: string | null;
   email: string;
   resetPasswordToken?: string | null;
   resetPasswordExpiration?: string | null;
@@ -54,6 +35,46 @@ export interface User {
   lockUntil?: string | null;
   password: string | null;
 }
+/**
+ * This interface was referenced by `Config`'s JSON-Schema
+ * via the `definition` "pages".
+ */
+export interface Page {
+  id: string;
+  page: {
+    slug?: string | null;
+    published?: boolean | null;
+  };
+  content: {
+    layout?: {
+      root: {
+        children: {
+          type: string;
+          version: number;
+          [k: string]: unknown;
+        }[];
+        direction: ('ltr' | 'rtl') | null;
+        format: 'left' | 'start' | 'center' | 'right' | 'end' | 'justify' | '';
+        indent: number;
+        type: string;
+        version: number;
+      };
+      [k: string]: unknown;
+    } | null;
+  };
+  meta?: {
+    title?: string | null;
+    description?: string | null;
+  };
+  title?: string | null;
+  updatedAt: string;
+  createdAt: string;
+  _status?: ('draft' | 'published') | null;
+}
+/**
+ * This interface was referenced by `Config`'s JSON-Schema
+ * via the `definition` "media".
+ */
 export interface Media {
   id: string;
   alt: string;
@@ -92,135 +113,10 @@ export interface Media {
     };
   };
 }
-export interface Page {
-  id: string;
-  page: {
-    slug?: string | null;
-    icon?:
-      | (
-          | 'codebracket'
-          | 'magnifyingglass'
-          | 'shoppingbag'
-          | 'atsymbol'
-          | 'megaphone'
-          | 'newspaper'
-          | 'flag'
-          | 'shieldexclamation'
-        )
-      | null;
-    published?: boolean | null;
-    haveHero?: boolean | null;
-    hero?: {
-      backgroundvideo?: string | Media | null;
-      announcement?: boolean | null;
-      announcementlink?: string | null;
-      announcementtext?: string | null;
-      title?: string | null;
-      subtitle?: string | null;
-      cta?: boolean | null;
-      ctalink?: string | null;
-      ctatext?: string | null;
-    };
-  };
-  content: {
-    layout?: {
-      root: {
-        children: {
-          type: string;
-          version: number;
-          [k: string]: unknown;
-        }[];
-        direction: ('ltr' | 'rtl') | null;
-        format: 'left' | 'start' | 'center' | 'right' | 'end' | 'justify' | '';
-        indent: number;
-        type: string;
-        version: number;
-      };
-      [k: string]: unknown;
-    } | null;
-  };
-  meta?: {
-    title?: string | null;
-    description?: string | null;
-  };
-  title?: string | null;
-  updatedAt: string;
-  createdAt: string;
-}
-export interface BlogPost {
-  id: string;
-  title?: string | null;
-  slug?: string | null;
-  published?: boolean | null;
-  hasheaderimage?: boolean | null;
-  headerimage?: string | Media | null;
-  excerpt?: string | null;
-  category?: (string | null) | BlogCategory;
-  body?: {
-    root: {
-      children: {
-        type: string;
-        version: number;
-        [k: string]: unknown;
-      }[];
-      direction: ('ltr' | 'rtl') | null;
-      format: 'left' | 'start' | 'center' | 'right' | 'end' | 'justify' | '';
-      indent: number;
-      type: string;
-      version: number;
-    };
-    [k: string]: unknown;
-  } | null;
-  author?: (string | null) | User;
-  meta?: {
-    title?: string | null;
-    description?: string | null;
-  };
-  updatedAt: string;
-  createdAt: string;
-}
-export interface BlogCategory {
-  id: string;
-  title?: string | null;
-  slug?: string | null;
-}
-export interface Partner {
-  id: string;
-  name: string;
-  url?: string | null;
-  image: string | Media;
-  updatedAt: string;
-  createdAt: string;
-}
-export interface File {
-  id: string;
-  title?: string | null;
-  updatedAt: string;
-  createdAt: string;
-  url?: string | null;
-  filename?: string | null;
-  mimeType?: string | null;
-  filesize?: number | null;
-  width?: number | null;
-  height?: number | null;
-}
-export interface NewsletterContact {
-  id: string;
-  email: string;
-  consent?: boolean | null;
-  updatedAt: string;
-  createdAt: string;
-}
-export interface Message {
-  id: string;
-  firstname: string;
-  lastname: string;
-  email: string;
-  phone: string;
-  message: string;
-  updatedAt: string;
-  createdAt: string;
-}
+/**
+ * This interface was referenced by `Config`'s JSON-Schema
+ * via the `definition` "payload-preferences".
+ */
 export interface PayloadPreference {
   id: string;
   user: {
@@ -240,68 +136,16 @@ export interface PayloadPreference {
   updatedAt: string;
   createdAt: string;
 }
+/**
+ * This interface was referenced by `Config`'s JSON-Schema
+ * via the `definition` "payload-migrations".
+ */
 export interface PayloadMigration {
   id: string;
   name?: string | null;
   batch?: number | null;
   updatedAt: string;
   createdAt: string;
-}
-export interface Meta {
-  id: string;
-  description: string;
-  keywords: string;
-  contactemail: string;
-  contactnumber: string;
-  updatedAt?: string | null;
-  createdAt?: string | null;
-}
-export interface Megamenu {
-  id: string;
-  sections?:
-    | {
-        type: 'link' | 'menu';
-        link: {
-          type?: ('reference' | 'custom') | null;
-          reference?: {
-            relationTo: 'pages';
-            value: string | Page;
-          } | null;
-          url?: string | null;
-          label: string;
-          newTab?: boolean | null;
-        };
-        name?: string | null;
-        page?: (string | Page)[] | null;
-        id?: string | null;
-      }[]
-    | null;
-  updatedAt?: string | null;
-  createdAt?: string | null;
-}
-export interface Footer {
-  id: string;
-  sections?:
-    | {
-        name: string;
-        page: (string | Page)[];
-        id?: string | null;
-      }[]
-    | null;
-  updatedAt?: string | null;
-  createdAt?: string | null;
-}
-export interface SocialMedia {
-  id: string;
-  socialmedia?:
-    | {
-        icon?: ('instagram' | 'facebook' | 'youtube' | 'linkedin' | 'twitter' | 'github') | null;
-        href?: string | null;
-        id?: string | null;
-      }[]
-    | null;
-  updatedAt?: string | null;
-  createdAt?: string | null;
 }
 
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ["./src/app/**/*.{js,ts,jsx,tsx}", "./src/components/**/*.{js,ts,jsx,tsx}"],
+	content: ["./src/app/**/*.{js,ts,jsx,tsx}", "./src/_components/**/*.{js,ts,jsx,tsx}"],
 	theme: {
 		extend: {
 			fontFamily: {
